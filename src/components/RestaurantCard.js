6ad4b03e134f742a8adbd82c66c55691ac0cafcb
m-tauqueer <mohammadtuti655@gmail.com>
2026-02-13 22:22:10 +0530

    filter

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -16,15 +16,15 @@ const RestaurantCard = (props) => {
     <div
       className="res-card"
       style={{
-        backgroundColor: '#f0f0f0',
+        backgroundColor: "#f0f0f0",
       }}
     >
       <img
         className="res-logo"
-        src={{CDN_URL}+cloudinaryImageId}
+        src={CDN_URL + cloudinaryImageId}
         alt="Biryani"
       />
-      
+
       {/* <h3>{props.resName}</h3>
         <h4>{props.cuisine}</h4> */}
       {/* <h3>{resData.data.name}</h3>
@@ -33,14 +33,12 @@ const RestaurantCard = (props) => {
       <h4>₹{resData.data.costForTwo / 100} FOR TWO</h4>
       <h4>{resData.data.deliveryTime} minutes</h4> */}
       <h3>{name}</h3>
-      <h4>{cuisines.join(', ')}</h4>
+      <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>₹{costForTwo / 100} FOR TWO</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   );
 };
-
-
 
 export default RestaurantCard;
